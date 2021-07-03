33a884249639c58910b914061f0c0fbda7e63125
mohamed-elmahdi <mohamed.elmahdi@virgingates.com>
2021-07-03 23:38:00 +0200

    Implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      countryName: null,
+      isValid: "All",
+      isQueried: false
+    }
+  }
+
+  sendApiCall = () => {
+    var baseLink = 'http://localhost:8088/api/v1/phone-numbers'
+    var countryName = this.state.countryName.value
+    var isValid = this.state.isValid.value
+    if(countryName !== "" || isValid !== "All"){
+      baseLink = baseLink.concat("?")
+    }
+    if(countryName !== ""){
+      baseLink = baseLink.concat("countryName=" + countryName)
+      this.state.isQueried = true
+    }
+    if(this.state.isQueried){
+      baseLink = baseLink.concat("&")
+    }
+    if(isValid !== "All"){
+      if(isValid === "Valid"){
+        baseLink = baseLink.concat("isValid=" + true)
+      } else {
+        baseLink = baseLink.concat("isValid=" + false)
+      }
+    }
+    this.state.isQueried = false
+
+    fetch(baseLink)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
+
+  render(){
+    var {items} = this.state;
+    return (
+      <div className="App">
+        <input name="Country" ref={(input) => this.state.countryName = input}></input>
+        <select name="isValid" ref={(input) => this.state.isValid = input}>
+          <option>Valid</option>
+          <option>Invalid</option>
+          <option>All</option>
+        </select>
+        <button onClick={this.sendApiCall}>Submit</button>
+        <ul>
+          {items.map(item => (
+            <li>
+              CountryCode: {item.countryCode}  |  Number: {item.number}  |  Country: {item.country}  |  Valid: {item.valid.toString()}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
